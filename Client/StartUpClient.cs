7073bd8f16abd50ec12793edea86cba08715fae4
Deyan Stoyanov <45766442+deyanstoyanov10@users.noreply.github.com>
2019-08-17 23:59:55 +0300

    Add files via upload

---
+++ Client/StartUpClient.cs
@@ -0,0 +1,75 @@
+﻿namespace ChatConsole
+{
+    using System;
+    using System.IO;
+    using System.Net;
+    using System.Net.Sockets;
+
+    public class StartUpClient
+    {
+        public static void Main(String[] args)
+        {
+            IPAddress ip_address = IPAddress.Parse("127.0.0.1");
+            int port = 8080;
+            try
+            {
+                if (args.Length >= 1)
+                {
+                    ip_address = IPAddress.Parse(args[0]);
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid IP address entered. Using default IP of: "
+                                                    + ip_address.ToString());
+            }
+            try
+            {
+                Console.WriteLine("Attempting to connect to server at IP address: {0} port: {1}",
+                                                    ip_address.ToString(), port);
+                TcpClient client = new TcpClient(ip_address.ToString(), port);
+
+                if (client.Connected)
+                {
+                    Console.WriteLine("Connection successful!");
+                }
+
+                StreamReader reader = new StreamReader(client.GetStream());
+                StreamWriter writer = new StreamWriter(client.GetStream());
+
+                Console.Write("Enter a name: ");
+                string nameEnter = Console.ReadLine();
+                Console.WriteLine();
+                string name = nameEnter + "-> ";
+
+                string s = String.Empty;
+                while (!s.Equals("Exit"))
+                {
+                    Console.Write("Enter a message: ");
+                    s = Console.ReadLine();
+                    Console.WriteLine();
+                    writer.WriteLine(name + s);
+                    writer.Flush();
+                    if (!s.Equals("Exit"))
+                    {
+                        String server_string = reader.ReadLine();
+                        Console.WriteLine(server_string);
+                    }
+                    else
+                    {
+                        writer.WriteLine(nameEnter + " exit the chat.");
+                        writer.Flush();
+                        Console.WriteLine("You exit the chat.");
+                    }
+                }
+                reader.Close();
+                writer.Close();
+                client.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
---
+++ Server/StartUpServer.cs
@@ -0,0 +1,76 @@
+﻿namespace ChatConsole
+{
+    using System;
+    using System.IO;
+    using System.Net;
+    using System.Net.Sockets;
+    using System.Threading;
+
+    public class StartUpServer
+    {
+        public static void Main()
+        {
+            TcpListener listener = null;
+            try
+            {
+                listener = new TcpListener(IPAddress.Any, 8080);
+                listener.Start();
+                Console.WriteLine("Server started...");
+
+                while (true)
+                {
+                    Console.WriteLine("Waiting for incoming client connections...");
+                    TcpClient client = listener.AcceptTcpClient();
+                    Console.WriteLine("Accepted new client connection...");
+                    Thread t = new Thread(ProcessClientRequests);
+                    t.Start(client);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (listener != null)
+                {
+                    listener.Stop();
+                }
+            }
+        }
+
+        private static void ProcessClientRequests(object argument)
+        {
+            TcpClient client = (TcpClient)argument;
+
+            try
+            {
+                StreamReader reader = new StreamReader(client.GetStream());
+                StreamWriter writer = new StreamWriter(client.GetStream());
+                string s = String.Empty;
+
+                while (!(s = reader.ReadLine()).Equals("Exit") || (s == null))
+                {
+                    Console.WriteLine(s);
+                    writer.WriteLine(s);
+                    writer.Flush();
+                }
+                reader.Close();
+                writer.Close();
+                client.Close();
+                Console.WriteLine("Client connection closed!");
+            }
+            catch (IOException)
+            {
+                Console.WriteLine("Problem with client communication. Exiting thread.");
+            }
+            finally
+            {
+                if (client != null)
+                {
+                    client.Close();
+                }
+            }
+        }
+    }
+}
